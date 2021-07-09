eea99fbad1397d79cb277f44698d0bf54c8b510b
Tomas1876 <mnd3259@gmail.com>
2021-07-09 18:53:10 +0900

    리트코드 이진트리 순회

--- LeetCode/BinaryTreeInorderTraversal.java
+++ LeetCode/BinaryTreeInorderTraversal.java
@@ -1,3 +1,4 @@
+package Basic;
 /*
 
  94. Binary Tree Inorder Traversal (이진트리 중위순회)
@@ -35,10 +36,30 @@ class Tree{
 		
 		return node;
 	}
+	
+	//왼쪽 노드 먼저 출력
+	public void inorder(Node node) {
+		
+		//마지막 자식 node까지 갈 때까지
+		if(node != null) {
+			
+			//왼쪽 node를 계속 넣어주며 재귀호출
+			inorder(node.left);
+			
+			//왼쪽 끝까지 갔다면 자기 자신 출력
+			System.out.println(node.data);
+			
+			//그리고 오른쪽으로 돌린다
+			inorder(node.right);
+			
+		}
+		
+	}
+	
 }
 
 
-public class BinaryTreeInorderTraversal {
+public class BinaryTreeTraversal {
 
 	public static void main(String[] args) {
 		
@@ -52,6 +73,15 @@ public class BinaryTreeInorderTraversal {
 		//마지막 node부터 생성
 		Node n4 = t.makeNode(null, 4, null);
 		Node n5 = t.makeNode(null, 5, null);
+		
+		Node n2 = t.makeNode(n4, 2, n5);
+		Node n3 = t.makeNode(null, 3, null);
+		Node n1 = t.makeNode(n2, 1, n3);
+		
+		//이제 1을 루트로 만든다
+		t.setRoot(n1);
+		
+		t.postorder(t.getRoot());
 
 	}
 
